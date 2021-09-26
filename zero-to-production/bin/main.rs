8b4f1dca402b4e86ac3c2b0fa6d56c12f1541a5c
ytakasugi <sh7.tibi0129@gmail.com>
2021-09-26 21:45:50 +0900

    change 3 files

--- zero-to-production/bin/main.rs
+++ zero-to-production/bin/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use zero_to_production::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = TcpListener::bind("127.0.0.1:8080")?;
+    run(address)?.await
 }
--- zero-to-production/tests/health_check.rs
+++ zero-to-production/tests/health_check.rs
@@ -1,10 +1,13 @@
+use std::net::TcpListener;
+
+
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-            .get("http://127.0.0.1:8000/health_check")
+            .get(&format!("{}/health_check", &address))
             .send()
             .await
             .expect("Failed to execute request.");
@@ -13,8 +16,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero_to_production::run().expect("Faild to bind address.");
-
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // OSから割り当てられたポートを回収する
+    let port = listener.local_addr().unwrap().port();
+    let server = zero_to_production::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // アプリケーションのアドレスを発信者に返します。
+    format!("http://127.0.0.1:{}", port)
 }
