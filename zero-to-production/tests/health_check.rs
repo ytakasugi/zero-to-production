4e9cbd22b7ad38c98b957012468bd8ece83757be
ytakasugi <sh7.tibi0129@gmail.com>
2021-10-21 23:13:44 +0900

    change 1 file

--- zero-to-production/tests/health_check.rs
+++ zero-to-production/tests/health_check.rs
@@ -6,7 +6,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // OSから割り当てられたポートを回収する
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     // アプリケーションのアドレスを発信者に返します。
     format!("http://127.0.0.1:{}", port)
@@ -27,3 +27,24 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_from_data() {
+    let app_address = spawn_app();
+    // 空のClientインスタンスを定義
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        // URLへのPOSTリクエストを行う
+        .post(&format!("{}/subscriptions", &app_address))
+        // リクエストにヘッダーを追加
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        // リクエストボディを設定
+        .body(body)
+        // Requestを構築し、ターゲットのURLに送信し、Responseを返す
+        .send()
+        .await
+        .expect("Failed to execute reauwest");
+
+    assert_eq!(200, response.status().as_u16());
+}
